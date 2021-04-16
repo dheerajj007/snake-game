bb7ae52de9a5e39ed75bbcec6d6597507d58777b
Dheeraj Kumar <dheerajkumarr235@gmail.com>
2021-04-16 10:21:47 +0530

    day1#completed

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
     
 gameIsOn = True 
 
