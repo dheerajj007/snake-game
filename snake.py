96ff949dc42b5fddf0523241a90874841bf475b5
Dheeraj Kumar <dheerajkumarr235@gmail.com>
2021-04-16 10:05:33 +0530

    snakeCLass#added control def

--- snake.py
+++ snake.py
@@ -4,11 +4,11 @@ STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
 class Snake:
-
     
     def __init__(self):
         self.segments = []
         self.createSnake()
+        self.head = self.segments[0]    
         
     def createSnake(self):
         for postion in STARTING_POSITION:
@@ -18,10 +18,21 @@ class Snake:
             segment.goto(postion)
             self.segments.append(segment)
     
-    
     def move(self):
         for segNum in range(len(self.segments)-1, 0, -1):
             newX = self.segments[segNum-1].xcor()
             newY = self.segments[segNum-1].ycor()
             self.segments[segNum].goto(newX, newY)
-        self.segments[0].forward(MOVE_DISTANCE)    
+        self.head.forward(MOVE_DISTANCE)    
+        
+    def up(self):
+        self.head.setheading(90)
+        
+    def down(self):
+        self.head.setheading(270)
+        
+    def left(self):
+        self.head.setheading(180)
+        
+    def right(self):
+        self.head.setheading(0)
