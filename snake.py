86a6d2c9ae7bf79311c9322ea42752279e811da2
Dheeraj Kumar <dheerajkumarr235@gmail.com>
2021-04-16 10:21:15 +0530

    snakeClass#changed var in constants

--- snake.py
+++ snake.py
@@ -2,7 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-
+UP=90
+DOWN=270
+RIGHT=180
+LEFT=0
 class Snake:
     
     def __init__(self):
@@ -25,18 +28,18 @@ class Snake:
             self.segments[segNum].goto(newX, newY)
         self.head.forward(MOVE_DISTANCE)    
         
-    def up(self):
-        if self.head.heading() != 270:
-            self.head.setheading(90)
+    def up(self):  
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
         
     def down(self):
-        if self.head.heading() != 90:
-            self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
         
     def left(self):
-        if self.head.heading() != 0:
-            self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
         
     def right(self):
-        if self.head.heading() != 180:
-            self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
